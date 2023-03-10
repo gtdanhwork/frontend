7c50a223193c59ac9d8c9b664b42fff38ce16b34
Giảng Thanh Danh <44111969+GTDANH@users.noreply.github.com>
2023-03-10 23:58:07 +0700

    Learning Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="siteContainer">
+      <header>
+        <div className="navHeader">
+          <h1>TMA</h1>
+          <div className="navBars">
+            <div className="navHome">Home</div>
+            <div className="navCategories">Categories</div>
+            <div className="navContact">Contact</div>
+          </div>
+          <h3>Account</h3>
+        </div>
+        <div className="formSearch">
+          <div className="formSearchBar">
+            <div className="formSearchInput">
+              <div className="formInput">
+                <div className="formDropdown">All Categories V</div>
+                <div className="formVerticalDiv" />
+                <input
+                  className="formInputKey"
+                  placeholder="Search Product, categories..."
+                ></input>
+                <button className="formSearchIcon">
+                  <img src="magnifier.png" alt="Search" />
+                </button>
+              </div>
+              <button className="formCartIcon">
+                <img src="cart.png" alt="Check Out" />
+              </button>
+            </div>
+          </div>
+        </div>
       </header>
+      <main>Tester</main>
+      <footer>
+        <div className="leftFooter">
+          <p>TMA</p>
+          <p>
+            Get out there & discover your <br /> laptop
+          </p>
+          <p>
+            Copyright 2019 TMA, Inc. Terms &<br />
+            Privacy
+          </p>
+        </div>
+        <div className="rightFooter">
+          <div className="companyInfo">
+            <h2>More on The Company</h2>
+            <p>About TMA</p>
+            <p>Contributions & Writers</p>
+            <p>Write For US</p>
+            <p>Contact Us</p>
+            <p>Privacy</p>
+          </div>
+          <div className="tmaInfo">
+            <h2>More on TMA</h2>
+            <p>The Team</p>
+            <p>Jobs</p>
+            <p>Press</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
